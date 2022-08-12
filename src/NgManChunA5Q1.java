6a0dc00fd981fa3f77a085637af6b5506bd036a8
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2022-08-12 16:57:19 -0500

    Q1: Initialize file

--- src/NgManChunA5Q1.java
+++ src/NgManChunA5Q1.java
@@ -1,5 +1,17 @@
-public class App {
+public class NgManChunA5Q1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+}
+
+class Patient {
+
+}
+
+class Node {
+
+}
+
+class PriorityQueue {
+
 }
